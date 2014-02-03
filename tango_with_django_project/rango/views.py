51bcd089e3694fec1bd039b784117f03a651c844
ueg1990 <uehtesham90@gmail.com>
2014-02-02 20:47:45 -0500

    Added about view

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Rango says Hello World!!!")
+
+def about(request):
+	return HttpResponse("Rango says: Welcome to the About page!!!")
