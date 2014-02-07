d331ddc1cbebb64d2e0512918598b7c39cb6184c
ueg1990 <uehtesham90@gmail.com>
2014-02-07 00:50:07 -0500

    Added hyperlinks to categories to access pages

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
 	# Obtain the context from the HTTP request
@@ -13,11 +13,33 @@ def index(request):
     # Place the list in our context_dict dictionary which will be passed to the template engine.
 
 	category_list = Category.objects.order_by('-likes')[:5]
+	for category in category_list:
+		category.url = category.name.replace(' ', '_')
 	context_dict = {'categories' : category_list}
 	return render_to_response('rango/index.html', context_dict, context)
-
 
 def about(request):
 	context = RequestContext(request)
 	context_dict = {'django' : 'DJANGO'}
 	return render_to_response('rango/about.html', context_dict, context)
+
+def category(request, category_name_url):
+	context = RequestContext(request)
+
+	category_name = category_name_url.replace('_', ' ')
+	context_dict = {'category_name' : category_name}
+	try:
+		category = Category.objects.get(name=category_name)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		pass
+	return render_to_response('rango/category.html', context_dict, context)
+
+
+
+
+
+
+
