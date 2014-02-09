ac667c4f23e64bb86d659131b01862b92ad4edab
ueg1990 <uehtesham90@gmail.com>
2014-02-09 01:17:31 -0500

    Fixed errors when adding Add Page option

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -19,11 +19,11 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
 
-	category_list = Category.objects.order_by('-likes')[:5]
+	category_list = Category.objects.order_by('-likes')[:10]
 	for category in category_list:
 		#category.url = category.name.replace(' ', '_')
 		category.url = encode(category.name)
-	page_list = Page.objects.order_by('-views')[:5]
+	page_list = Page.objects.order_by('-views')[:10]
 	context_dict = {'categories' : category_list, 'pages' : page_list}
 	return render_to_response('rango/index.html', context_dict, context)
 
